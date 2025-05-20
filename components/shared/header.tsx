3d7864c00400dd3da3424f3d2daa9675c3a7f0e8
0v3nb1rd <0v3nb1rd@gmail.com>
2025-05-20 15:17:16 +0300

    refactor(Header)

--- components/shared/header.tsx
+++ components/shared/header.tsx
@@ -10,15 +10,27 @@ export const Header = () => {
       <div className="container">
         <div className="flex items-center justify-between py-4">
           <Link href="/" className="logo">
-            Work - E
+            <i>Work- E</i>
           </Link>
           <div className="flex items-center gap-2">
-            <ul>
-              <li>УКР</li>
-            </ul>
+            <div className="flex items-center gap-2">
+              <Button variant="secondary">Увійти</Button>
+              <Button variant="secondary">Зареєструватись</Button>
+            </div>
 
-            <Button variant="secondary">Увійти</Button>
-            <Button variant="secondary">Зареєструватись</Button>
+            <ul className="ml-4 flex items-center gap-4">
+              <li>
+                <a href="#" className="flex items-center gap-2">
+                  <i>
+                    <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M16 31C24.3 31 31 24.3 31 16H1C1 24.3 7.7 31 16 31Z" fill="#FFE62E" />
+                      <path d="M16 1C7.7 1 1 7.7 1 16H31C31 7.7 24.3 1 16 1Z" fill="#428BC1" />
+                    </svg>
+                  </i>
+                  УКР
+                </a>
+              </li>
+            </ul>
           </div>
         </div>
       </div>
