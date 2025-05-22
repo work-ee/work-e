3fa931aaf7f5fbbb1c8e496ced315af9638032ee
nadiiahonchar1 <nadiiahonchar1@gmail.com>
2025-05-22 15:53:50 +0300

    add radiobutton

--- components/ui/index.ts
+++ components/ui/index.ts
@@ -1,3 +1,4 @@
 export { Button } from "@/components/ui/Button";
 export { Input } from "@/components/ui/Input";
 export { Checkbox } from "@/components/ui/Checkbox";
+export { RadioButton } from "@/components/ui/Radiobutton";
