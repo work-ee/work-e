b05089ed455410c5580d8c4cdd6d5fc40ea71e4f
Svitlana-G <svitlana.golubieva@gmail.com>
2025-05-26 17:12:49 +0200

    Add styles

--- components/modals/LinkedInPopup.tsx
+++ components/modals/LinkedInPopup.tsx
@@ -1,13 +1,15 @@
+import "../../app/globals.css";
+
 interface LinkedInPopupProps {
   onClose: () => void;
 }
 
 export default function LinkedInPopup({ onClose }: LinkedInPopupProps) {
   return (
-    <div>
-      <div>
-        <button onClick={onClose} aria-label="Close">
-          ×
+    <div className="fixed inset-0 flex items-center justify-center bg-black/40 z-100 ">
+      <div className="w-[600px] rounded-[24px] p-6 shadow-xl bg-white relative">
+        <button className="absolute top-3 right-0" onClick={onClose} aria-label="Close">
+          <img src="img/close.svg" />
         </button>
         <p>
           Підключи LinkedIn, щоб ми могли зчитати твою професійну діяльність і заповнити твій профіль.
@@ -15,9 +17,9 @@ export default function LinkedInPopup({ onClose }: LinkedInPopupProps) {
           Ми не використовуємо ці дані ні для чого іншого лише для створення твого резюме та покращення результатів
           пошуку.
         </p>
-        <div>
-          <button>LinkedIn</button>
-          <button>Немає LinkedIn</button>
+        <div className="flex justify-between mt-6">
+          <button className="btn underline ">LinkedIn</button>
+          <button className="btn">Немає LinkedIn</button>
         </div>
       </div>
     </div>
