c053756135de8ca0d0b15fa20360f31963e43eac
Svitlana-G <svitlana.golubieva@gmail.com>
2025-06-05 16:52:10 +0200

    Add reusable LinkedinPopupTemlate component

---
+++ components/feedback/LinkedInPopup/LinkedInPopupTemplate.tsx
@@ -0,0 +1,26 @@
+import { Button } from "@/components/ui";
+
+import { Modal } from "../Modal";
+
+interface LinkedInPopupProps {
+  onClose: () => void;
+  title?: string;
+  text: string;
+}
+
+export const LinkedInPopupTemplate = ({ onClose, title, text }: LinkedInPopupProps) => {
+  <Modal
+    onClose={onClose}
+    title={title}
+    actions={
+      <div className="flex gap-18 w-full mt-4">
+        <Button variant="secondary" className="flex-1 flex justify-center">
+          Немає LinkedIn
+        </Button>
+        <Button className="flex-1 flex justify-center">LinkedIn</Button>
+      </div>
+    }
+  >
+    <p className="text-neutral-900 pr-15">{text}</p>
+  </Modal>;
+};
