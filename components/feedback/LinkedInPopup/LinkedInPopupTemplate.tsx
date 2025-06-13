3c3782b934b3b9197d258403f6985ec8a53675b9
Svitlana-G <svitlana.golubieva@gmail.com>
2025-06-13 15:32:38 +0200

    Add LinkedInPopupTemplate component

--- components/feedback/LinkedInPopup/LinkedInPopupTemplate.tsx
+++ components/feedback/LinkedInPopup/LinkedInPopupTemplate.tsx
@@ -9,18 +9,20 @@ interface LinkedInPopupProps {
 }
 
 export const LinkedInPopupTemplate = ({ onClose, title, text }: LinkedInPopupProps) => {
-  <Modal
-    onClose={onClose}
-    title={title}
-    actions={
-      <div className="flex gap-18 w-full mt-4">
-        <Button variant="secondary" className="flex-1 flex justify-center">
-          Немає LinkedIn
-        </Button>
-        <Button className="flex-1 flex justify-center">LinkedIn</Button>
-      </div>
-    }
-  >
-    <p className="text-neutral-900 pr-15">{text}</p>
-  </Modal>;
+  return (
+    <Modal
+      onClose={onClose}
+      title={title}
+      actions={
+        <div className="flex gap-18 w-full mt-4">
+          <Button variant="secondary" className="flex-1 flex justify-center">
+            Немає LinkedIn
+          </Button>
+          <Button className="flex-1 flex justify-center">LinkedIn</Button>
+        </div>
+      }
+    >
+      <p className="text-neutral-900 pr-15">{text}</p>
+    </Modal>
+  );
 };
