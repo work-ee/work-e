effdacacf28cb68c3671b00e9dbf9cfb6d28fd1f
Svitlana-G <svitlana.golubieva@gmail.com>
2025-06-13 15:34:28 +0200

    Update LinkedInShowPopup tu use template component and delete LinkedInPopup

--- components/feedback/LinkedInPopup/LinkedInShowPopup.tsx
+++ components/feedback/LinkedInPopup/LinkedInShowPopup.tsx
@@ -2,10 +2,24 @@
 
 import { useState } from "react";
 
-import LinkedInPopup from "@/components/feedback/LinkedInPopup/LinkedInPopup";
+import { LinkedInPopupTemplate } from "./LinkedInPopupTemplate";
 
-export default function LinkedInSection() {
-  const [show, setShow] = useState(true);
-
-  return show ? <LinkedInPopup onClose={() => setShow(false)} /> : null;
+interface LinkedInShowPopupProps {
+  variant: "default" | "withHeader";
 }
+
+export const LinkedInShowPopup = ({ variant }: LinkedInShowPopupProps) => {
+  const [show, setShow] = useState(true);
+  const baseText =
+    "Підключи LinkedIn, щоб ми могли зчитати твою професійну діяльність і заповнити твій профіль.\nМи не використовуємо ці дані ні для чого іншого — лише для створення твого резюме та покращення результатів пошуку.";
+  const extendedText =
+    "Без LinkedIn нам важко зрозуміти твій досвід. Додай його, коли будеш готовий — це відкриє більше релевантних вакансій!";
+  if (!show) return null;
+  return (
+    <LinkedInPopupTemplate
+      onClose={() => setShow(false)}
+      title={variant === "withHeader" ? "Додайте LinkedIn" : undefined}
+      text={variant === "withHeader" ? extendedText : baseText}
+    />
+  );
+};
