62e42c5db02303fabae15df15632f68c4260b51f
0v3nb1rd <0v3nb1rd@gmail.com>
2025-07-30 07:49:45 +0300

    fix: improve conditional rendering and animation for more button in CardList component

--- components/onboarding/CardList.tsx
+++ components/onboarding/CardList.tsx
@@ -19,10 +19,10 @@ export const CardList = ({ length = 3, muted = false, moreBtn = false, data = []
         {length === 0 && <li className="text-center text-gray-500">No cards available...</li>}
       </ul>
 
-      {moreBtn && (
+      {moreBtn && data.length > length && (
         <Link
           href="/jobs"
-          className="hover:bg-secondary-50 ml-4 flex cursor-pointer items-center rounded-2xl px-4 py-6 transition-shadow hover:shadow-[0_0_0_2px_var(--color-primary-100)]"
+          className="hover:bg-secondary-50 animate-in fade-in-0 slide-in-from-right-5 relative ml-4 flex cursor-pointer items-center rounded-2xl px-4 py-6 transition-all ease-in-out hover:shadow-[0_0_0_2px_var(--color-primary-100)]"
         >
           <ArrowRightSvg />
         </Link>
